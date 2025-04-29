e06a49bcc4355325bcf92466e8e2a5d3b6ac064a
marco-gulino <marcogulino98@gmail.com>
2025-04-29 18:30:05 +0200

    [feat] concatenate variables in  PasswordGenerator to create the pwd variable

--- src/org/lessons/java/security/PasswordGenerator.java
+++ src/org/lessons/java/security/PasswordGenerator.java
@@ -1,11 +1,47 @@
 package org.lessons.java.security;
 
+import java.util.Scanner;
+
 public class PasswordGenerator {
-    // Vars
-    String name;
-    String surname;
-    String favColor;
-    Byte day;
-    Byte month;
-    short year;
+    public static void main(String[] args) {
+        // Vars
+        String name;
+        String surname;
+        String favColor;
+        Byte day;
+        Byte month;
+        short year;
+
+        String pwd;
+
+        // New Scanner instance
+        Scanner input = new Scanner(System.in);
+
+        // Get Input from User
+        System.out.println("Insert Name...");
+        name = input.nextLine();
+
+        System.out.println("Insert Surname...");
+        surname = input.nextLine();
+
+        System.out.println("Insert favourite color...");
+        favColor = input.nextLine();
+
+        System.out.println("Insert birth day...");
+        day = input.nextByte();
+
+        System.out.println("Insert birth month...");
+        month = input.nextByte();
+
+        System.out.println("Insert birth year...");
+        year = input.nextShort();
+
+        pwd = String.format("%s-%s-%s-%d", name, surname, favColor, day + month + year);
+
+        // DEBUG
+        System.out.println(name + " " + surname + " " + favColor + " " + day + " " + month + " " + year);
+
+        // Print Pwd
+        System.out.println("Ecco la tua password: " + pwd);
+    }
 }
