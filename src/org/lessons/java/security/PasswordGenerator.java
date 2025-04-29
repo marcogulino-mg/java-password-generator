b1a7594723b49e643325162b96e3ffc3c437a25e
marco-gulino <marcogulino98@gmail.com>
2025-04-29 17:27:28 +0200

    [feat] add variables to PasswordGenerator class

--- src/org/lessons/java/security/PasswordGenerator.java
+++ src/org/lessons/java/security/PasswordGenerator.java
@@ -1,5 +1,11 @@
 package org.lessons.java.security;
 
 public class PasswordGenerator {
-
+    // Vars
+    String name;
+    String surname;
+    String favColor;
+    Byte day;
+    Byte month;
+    short year;
 }
